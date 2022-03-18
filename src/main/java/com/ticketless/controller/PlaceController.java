f907520ae6d424ce1130faed1d02d9f0ccb84873
jainnneel <jainneel9933@gmail.com>
2022-03-18 10:44:01 +0530

    fianl2

--- src/main/java/com/ticketless/controller/PlaceController.java
+++ src/main/java/com/ticketless/controller/PlaceController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +25,8 @@ public class PlaceController {
     @Autowired
     private PlaceService placeService;
     
-    @GetMapping(value = "/place/search/{pageno}",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getPlaceByStateAndCity(@ModelAttribute @RequestBody StateCitySearch stateCitySearch,@PathVariable("pageno") int pageNo) {
+    @PostMapping(value = "/place/search/{pageno}",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getPlaceByStateAndCity(@PathVariable("pageno") int pageNo,@RequestBody StateCitySearch stateCitySearch) {
         ResponseEntity entity = new ResponseEntity();
         List<PlaceResponseDto> placeResponseDtos = placeService.getAllPlaceyStateCity(stateCitySearch,pageNo);    
         entity.setData(placeResponseDtos);
